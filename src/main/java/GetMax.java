dcd4a2d47e0afa46d54959de0244e40de609fe87
Kevin Moore <kevinmoore444@gmail.com>
2023-06-08 02:09:30 +0000

    Completed getmax exercise

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,15 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(i==0){
+                max = arr[i];
+            }
+            else if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
     }
 }
